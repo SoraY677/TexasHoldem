dfc532037c8d4875c56fb5167667c0fa1d31634d
SoraY677 <SoraY677@outlook.com>
2020-05-14 18:17:03 +0900

    継承用のカード保持オブジェクトクラスを追加

---
+++ src/CardHolder.kt
@@ -0,0 +1,23 @@
+open class CardHolder {
+    val frame = GameFrame()
+    val BasePosition:MutableMap<String,Int> = mutableMapOf("x" to 0 , "y" to 0)
+
+    var cardList:ArrayList<TrumpCard> = arrayListOf()
+
+    constructor(){}
+
+
+    fun Init(){
+        cardList = arrayListOf()
+    }
+
+    /**
+     * 配られたカードを受け取る
+     */
+    fun recieveCards(receiveCardList:ArrayList<TrumpCard>){
+        receiveCardList.forEach {  recieveCard ->
+            cardList.add(recieveCard)
+        }
+
+    }
+}
